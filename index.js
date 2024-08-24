fb2aa3bb491f2502130e161ad536781467a77830
Akin Williams <aowilliams@arstropica.com>
2024-08-24 12:08:33 -0400

    Hermes check

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+const isHermes = () => !!global.HermesInternal;
+console.log('Is using Hermes: ', !!global.HermesInternal);
+
 AppRegistry.registerComponent(appName, () => App);
